e7cbb43fe342bfcc5f33c0ebb9db97e9c2c1eed4
André misuraca <m1sur4k@gmail.com>
2023-03-05 21:38:43 -0300

    Adicionar arquivos de projeto.

---
+++ temperatura/Program.cs
@@ -0,0 +1,11 @@
+﻿double celsius, fahrenheit;
+
+Console.WriteLine("digite a temperatura em graus celsius");
+celsius = double.Parse(Console.ReadLine());
+
+fahrenheit = (celsius * 9 / 5) + 32;
+
+Console.WriteLine("A temperatura em fahrenheit é: " + fahrenheit);
+
+
+
